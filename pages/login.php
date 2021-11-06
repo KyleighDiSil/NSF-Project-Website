60fa2a0db9420f80edda1b88b473fd807486d8f2
Clloyd3267 <37284048+Clloyd3267@users.noreply.github.com>
2021-11-06 16:21:03 -0400

    Updated login style and removed style.css

--- pages/login.php
+++ pages/login.php
@@ -5,31 +5,45 @@
     <main id="main">
         <div>
             <form action="../php-scripts/login.php" method="POST">
-                <label id="usernameLabel" for="username">Username:</label>
-                <input type="text" id="username" name="username" autocomplete="off">
-                <label id="passwordLabel" for="password">Password:</label>
-                <input type="password" id="password" name="password" autocomplete="off">
-                <div id="lower">
-                    <div id="showPassbox">
-                        <input type="checkbox" id="checkShowPass" onclick="showPass()">
-                        <label id="showPassLabel" for="checkShowPass">Show Password</label>
-                    </div>
-                    <input type="submit" id="submit" value="Login">
-                    <a href="./create_account.php"><p>Create Account</p></a>
+                <h1>Sign in</h1>
+                <input type="text" id="username" name="username" placeholder="Email" >
+                <div id="password-stuff">
+                    <input type="password" id="password" name="password" autocomplete="off" placeholder="Password" >
+                    <i class="fas fa-eye-slash" id="togglePassword" onclick="showPassword()"></i>
                 </div>
+                <a href=""><p>Forgot Password</p></a>
+                <input type="submit" id="submit" value="Login" >
+                <a href="./create_account.php"><p>Create Account</p></a>
             </form>
         </div>
     </main>
     <script src="http://code.jquery.com/jquery-2.1.4.js"></script>
     <script type="text/javascript">
-        function showPass() {
-            var x = document.getElementById("password");
-            if (x.type === "password") {
-                x.type = "text";
-            } else {
-                x.type = "password";
+        function showPassword() {
+            const togglePassword = document.querySelector('#togglePassword');
+            const password = document.querySelector('#password');
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+            if (togglePassword.getAttribute("class") == "fas fa-eye-slash")
+            {
+                togglePassword.setAttribute("class", "fas fa-eye");
+            }
+            else
+            {
+                togglePassword.setAttribute("class", "fas fa-eye-slash");
             }
         }
+
+
+        // const togglePassword = document.querySelector('#togglePassword');
+        // const password = document.querySelector('#password');
+        // togglePassword.addEventListener('click', function (e) {
+        //     // toggle the type attribute
+        //     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        //     password.setAttribute('type', type);
+        //     // toggle the eye / eye slash icon
+        //     togglePassword.toggleClass("fas fa-eye-slash");
+        // });
     </script>
 <?php require "../php-snippets/bottom_template.php"; ?>
 
