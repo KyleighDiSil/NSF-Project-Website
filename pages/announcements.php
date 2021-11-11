a29ed0f81ee2b38d0a9f14fcc2699ce55f9b8caf
KyleighDiSil <81115610+KyleighDiSil@users.noreply.github.com>
2021-11-11 12:19:43 -0500

    editing font-family and account_management

--- pages/announcements.php
+++ pages/announcements.php
@@ -4,6 +4,11 @@
 <?php require "../php-snippets/top_template.php"; ?>
 <?php if (!isset($_SESSION["loggedin"]) || !$_SESSION["loggedin"]) {header("Location: ../pages/home.php");} ?>
     <main id="main">
-        <h1>Header 1</h1>
+        <h1 id="page-title">Announcements</h1>
+        <div id="announcement-container">
+            <h2 id="title">Title of Announcement</h2>
+            <p id="date">Announcement Date</p>
+            <p id="content">The Content</p>
+        </div>
     </main>
 <?php require "../php-snippets/bottom_template.php"; ?>
--- php-snippets/footer.php
+++ php-snippets/footer.php
@@ -1,18 +1,4 @@
 <footer>
-    <div class="social-links">
-        <ul>
-            <li class="social-items">
-                <a href="https://linkedin.com/in/kyleigh-disilvestro-8aa044191" style="color: #0072b1;">
-                    <i class="fab fa-linkedin"></i>
-                </a>
-            </li>
-            <li class="social-items">
-                <a href="#" style="color: #1DB954;">
-                    <i class="fab fa-spotify"></i>
-                </a>
-            </li>
-        </ul>
-    </div>
     <div class="footer-links">
         <ul>
             <li><a href="">Return to Top</a></li>
