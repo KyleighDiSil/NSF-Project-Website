db8320f8dd5cb7811a6a79b7f838dbf6840c25fc
Owen Casciotti <orcasciotti@comcast.net>
2021-12-06 17:58:53 -0500

    Password updating checks inputs

--- php-scripts/update_password.php
+++ php-scripts/update_password.php
@@ -7,18 +7,21 @@
   // Validate inputs
   if ($_POST['old-pass'] == "")
   {
+    // No old password entered
     header("Location: ../pages/account_management.php#UpdateFailed");
   }
   if ($_POST['new-pass'] == "")
   {
+    // No new password entered
     header("Location: ../pages/account_management.php#UpdateFailed");
   }
   if ($_POST['new-pass'] != $_POST['confirm-new-pass'])
   {
+    // New password does not match confirmation of new password
     header("Location: ../pages/account_management.php#UpdateFailed");
   }
   $uid = $_SESSION['userID'];
-  $get_salt = "SELECT Salt FROM Users WHERE UserID = $uid;";
+  $get_salt = "SELECT (Salt Password) FROM Users WHERE UserID = $uid;";
   if (!$result = mysqli_query($conn, $get_salt))
   {
     echo "Error"; //Need better errors
@@ -28,11 +31,22 @@
     $row = $result->fetch_assoc();
     $Salt = $row['Salt'];
     $Split = str_split($Salt, 10);
-    $Salted = $Split[0] . $_POST["new-pass"] . $Split[1];
-    $Hashed = hash('sha256', $Salted);
-    $update_pass = "UPDATE Users SET Password= '$Hashed' WHERE UserID = $uid;";
-    mysqli_query($conn, $update_pass);
     
-    header("Location: ../pages/account_management.php#PasswordUpdated");
+    // Check if old password is correct
+    $old_hashed = hash('sha256', $Split[0] . $_POST["old-pass"] . $Split[1]);
+    if ($old_hashed != $row['Password'])
+    {
+      // Password doesn't match
+      header("Location: ../pages/account_management.php#UpdateFailed");
+    }
+    else
+    {
+      $Salted = $Split[0] . $_POST["new-pass"] . $Split[1];
+      $Hashed =a hash('sha256', $Salted);
+      $update_pass = "UPDATE Users SET Password= '$Hashed' WHERE UserID = $uid;";
+      mysqli_query($conn, $update_pass);
+      
+      header("Location: ../pages/account_management.php#PasswordUpdated");
+    }
   }
 ?>
